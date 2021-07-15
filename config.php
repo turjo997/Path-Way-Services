f361d5c0eeed6822da5d29e85715170bddd8e603
Ullash Bhattacharjee(Turjo) <53423899+turjo997@users.noreply.github.com>
2021-07-15 23:14:26 +0600

    Add files via upload

---
+++ config.php
@@ -0,0 +1,23 @@
+<?php
+/*
+  $host = "remotemysql.com";
+  $db = "6JBOpMPTpi" ; 
+  $user = "6JBOpMPTpi";
+  $pass = "za8u35Pbzw";
+  $charset = "utf8mb4";
+*/
+  $host = "remotemysql.com";
+  $db = "AkNRlVqE02" ; 
+  $user = "AkNRlVqE02";
+  $pass = "lG1bSBRz4H";
+  $charset = "utf8mb4";
+
+
+
+
+ //$link = mysqli_connect("localhost", "root" , "", "path-way services");
+
+ $link =  mysqli_connect ($host, $user , $pass , $db);
+
+ //$link = mysqli_connect($host , $db , $charset , $user , $pass );
+?>
