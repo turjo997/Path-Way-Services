bb2c27d4ee0076c52a6d5dce69b2dd9c0b3b6b4a
Ullash Bhattacharjee(Turjo) <53423899+turjo997@users.noreply.github.com>
2021-07-15 23:17:43 +0600

    Add files via upload

---
+++ user/main.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Path-Way Services</title>
+
+
+       <!--font awsome-->
+       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous"/>
+
+<link href="https://fonts.googleapis.com/css2?family=Handlee&family=Montserrat:wght@100&family=Roboto:wght@100&display=swap" rel="stylesheet">
+ <!---bootstrapt cdn--->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">	
+       
+           <style>
+
+
+          header{
+                  background: url("images/three-cover.jpg");
+                  background-position: center;
+                  background-attachment: fixed;
+                  background-repeat: no-repeat;
+                  background-size: cover;
+            }
+
+            .banner-container{
+                height: 100vh;
+                }
+                .banner-container h1{
+                font-size: 3.2rem;
+                background-color: rgb((0,0,128));
+                padding: 10px 20px;
+             
+                border-radius: 5px;
+                }
+
+
+           </style>
+   
+       <meta name="viewport" content="width = device-width , initial-scale = 1.0">
+       <meta http-equiv="X-UA-compatible" >
+  <meta charset="utf-8">
+  
+  <link rel="stylesheet" href="footerstyle.css">
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+</head>
+
+
+<body>
+
+
+
+
+  
+     <nav class="navbar navbar-expand-lg navbar-light bg-light py-5">
+ 
+ <div class="collapse navbar-collapse" id="navbarNavDropdown">
+   <ul class="navbar-nav">
+  
+   <li class="nav-item">
+       <a class="nav-link" href="contact.php"><b>Home</b></a>
+     </li>
+     <li class="nav-item">
+       <a class="nav-link" href="contact.php"><b>Login</b></a>
+     </li>
+     <li class="nav-item">
+       <a class="nav-link" href="contact.php"><b>Register</b></a>
+     </li>
+     <li class="nav-item">
+       <a class="nav-link" href="aboutus.php"><b>About Us</b></a>
+     </li>
+     <li class="nav-item dropdown">
+       <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <b> Categories </b>
+       </a>
+       <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+         <a class="dropdown-item" href="#bestseller"><b>Best Sellers</b></a>
+         <a class="dropdown-item" href="#">Novels</a>
+         <a class="dropdown-item" href="#actions">Actions & Adventures</a>
+         <a class="dropdown-item" href="#comics">Comics</a>
+         <a class="dropdown-item" href="#detactive">Detactive</a>
+       </div>
+     </li>
+   </ul>
+ </div>
+</nav>
+
+
+  
+
+  <header>
+     <div class="banner-container d-flex justify-content-center align-items-center">
+      <div class="banner-contents text-center">
+
+      <div class="row">
+       <div class="col-lg-4">
+       <h1 style="color:#a60692">Welcome to Path-Way E-Ticketing Services.</h1>
+ 
+
+       </div>
+
+      </div>
+    
+      </div>
+      </div>
+    
+     </header>
+
+
+   
+
+
+
+
+
+<?php
+   require_once 'footer.php';
+?>
