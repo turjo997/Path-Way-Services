e350a6b9692ecc698ac74888264ef3586c6e439d
Ullash Bhattacharjee(Turjo) <53423899+turjo997@users.noreply.github.com>
2021-07-18 20:12:19 +0600

    Add files via upload

---
+++ admin/profile.php
@@ -0,0 +1,22 @@
+<?php
+   require_once 'header.php';
+   require_once 'checklogin.php';
+?>
+
+<div class="container">
+
+<div class="div1 bg-success p-4 p-lg-3 m-2 m-lg-4">
+    <h3 class="text-black text-center">Welcome to the Admin Panel Arena !</h3>           
+</div>
+
+
+<section>
+
+   </section>
+
+                                             
+                             
+</div>
+<?php
+   require_once 'footer.php';
+?>
