6a3d038f25c6cc3a26defd48142fa93142cfde51
Ullash Bhattacharjee(Turjo) <53423899+turjo997@users.noreply.github.com>
2021-07-20 14:03:47 +0600

    Update profile.php

--- admin/profile.php
+++ admin/profile.php
@@ -1,22 +1,16 @@
 <?php
+
    require_once 'header.php';
    require_once 'checklogin.php';
 ?>
 
-<div class="container">
-
-<div class="div1 bg-success p-4 p-lg-3 m-2 m-lg-4">
-    <h3 class="text-black text-center">Welcome to the Admin Panel Arena !</h3>           
-</div>
 
 
-<section>
 
-   </section>
-
-                                             
-                             
-</div>
+<script src="profile.js"></script>  
+<br><br><br><br><br><br><br><br><br></br><br><br><br><br><br><br><br><br><br></br><br><br><br><br><br><br><br><br><br></br>
 <?php
    require_once 'footer.php';
 ?>
+
+
