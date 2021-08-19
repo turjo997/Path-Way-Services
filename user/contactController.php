64cd219b8dacc07249acf74d9faba49753083c8e
Ullash Bhattacharjee(Turjo) <53423899+turjo997@users.noreply.github.com>
2021-08-19 23:48:21 +0600

    Update contactController.php

--- user/contactController.php
+++ user/contactController.php
@@ -1,9 +1,15 @@
 <?php
 
+require_once '../config.php';
+
+/*
 use PHPMailer\PHPMailer\PHPMailer;
 require_once "class.phpmailer.php";
 require_once "class.smtp.php";
 require_once "class.exception.php";
+require_once "class.POP3.php";
+require_once "class.OAuth.php";
+//require_once "autoload.php";
 
 
 
@@ -24,7 +30,7 @@ $result="";
     $mail->SMTPAuth = true;
     $mail->SMTPSecure='tls';
     $mail->Username="turjouser98@gmail.com";
-    $mail->Password='98turjouser98';
+    $mail->Password='abcCoders987';
 
     $mail->addAddress('turjouser98@gmail.com');
     $mail->isHTML(true);
@@ -51,6 +57,73 @@ $result="";
 
 
   }
+  */
+
+
+
+
+
+$name = $mail =$sub=$msg=$status="";
+$name_err=$mail_err =$sub_err=$msg_err =$error ="";
+   
+   if(isset($_POST['submit'])){
+ 
+    if(empty($_POST['fullname'])){
+      $name_err  = "Please enter your name" ; 
+    }else{
+      $name = check_input($_POST['fullname']);
+   }
+   if(empty($_POST['email'])){
+    $mail_err  = "Please enter your mail" ; 
+  }else{
+    $mail = check_input($_POST['email']);
+ }
+          
+ if(empty($_POST['subject'])){
+  $sub_err  = "Please enter the subject" ; 
+}else{
+  $sub = check_input($_POST['subject']);
+}
+        
+if(empty($_POST['message'])){
+  $msg_err  = "Please type the message" ; 
+}else{
+  $msg = check_input($_POST['message']);
+}
+        
+            
+    if(empty($name_err) && empty($mail_err) && empty($sub_err) && empty($msg_err)){
+     
+      $sql = "INSERT INTO  contact (fullname,email,subject,message)   values('$name','$mail','$sub','$msg')";
+       
+       if (!$link) {
+        die("Connection failed: " . mysqli_connect_error());
+        }
+
+        if (mysqli_query($link, $sql)) {
+        
+          $status = '<div class="alert alert-success ">Thankyou! for contacting us, We\'ll get back to you soon!</div>';
+        } else {
+         
+          $status = '<div class="alert alert-danger ">Message not send. Try again.</div>' ; 
+       }
+       mysqli_close($link);
+  
+    }
+
+  }
+
+
+  function check_input($data){
+    $data =  trim($data);
+    $data =  stripcslashes($data);
+    $data =  htmlspecialchars($data);      
+    return $data;
+
+   }
+
+
+
 
 
  ?>
